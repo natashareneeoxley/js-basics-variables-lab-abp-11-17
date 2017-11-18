f8377cf4777181b8df6f160305b9e2a9955f2440
Natasha Oxley <natashareneeoxley@gmail.com>
2017-11-18 15:38:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 js
 const companyName = parseInt('Scuber');
+expect(companyName).to.equal('Scuber');
 let mostProfitableNeighborhood =  parseInt('Chelsea');
 
 let companyCeo = parseInt('Susan Smith');
